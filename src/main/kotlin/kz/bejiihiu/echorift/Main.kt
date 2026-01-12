cf90bc9ee6aa023e008467f138e0f36c54d89106
Tamerlan Tulepbergenov <bejiihiu@icloud.com>
2026-01-12 20:00:33 +0500

    init

---
+++ src/main/kotlin/kz/bejiihiu/echorift/Main.kt
@@ -0,0 +1,14 @@
+package kz.bejiihiu.echorift
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
